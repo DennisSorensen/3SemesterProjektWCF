b9e3cc490c682817f852c2c9fb68115a6d2d1088
Dennis Sørensen <dennissorensen24@hotmail.dk>
2017-11-09 12:36:02 +0100

    Lavet create user test

---
+++ WCFTest/CreateUserTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ClientTest
+{
+    [TestClass]
+    public class CreateUserTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            BookController bookController = new BookController();
+
+            //Act
+            bookController.Create("12345678910", "TestBook", "Gruppe 3", 99.95, 3);
+
+            Book testBook = new Book("12345678910", "TestBook", "Gruppe 3", 99.95, 3); //Opretter bog objekt til og tjekke med.
+
+            //Assert
+            Book book = bookController.Find("12345678910");
+            Assert.ReferenceEquals(book, testBook); //Tjekker om de to objekter er ens.
+            //Assert.AreEqual("TestBook", book.Title);  Er kun til numeriske værdier.
+
+        }
+
+        [TestMethod]
+        public void TestCreateUser()
+        {
+            //Arrange
+            UserController userController = new UserController();
+
+            //Act
+
+            //Assert
+
+        }
+    }
+}
