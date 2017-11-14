d5a32b91eef6d7176d92884f5bd00a50bf6b658d
Bodil <bodil.noergaard86@gmail.com>
2017-11-14 10:45:28 +0100

    create Calendar DB layer

---
+++ WCF.DatabaseAccessLayer/CalendarDb.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using WCF.ModelLayer;
+
+namespace WCF.DatabaseAccessLayer
+{
+    public class CalendarDb : IDbCrud<Calendar>
+    {
+        private readonly string CONNECTION_STRING = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        public void Create(Calendar calendar)
+        {
+            TransactionOptions to = new TransactionOptions { IsolationLevel = IsolationLevel.RepeatableRead };
+            using (TransactionScope scope = new TransactionScope(TransactionScopeOption.RequiresNew, to))
+            {
+                using (SqlConnection connection = new SqlConnection(CONNECTION_STRING))
+                {
+                    connection.Open();
+
+
+                    try
+                    {
+                        using (SqlCommand cmd = connection.CreateCommand())
+                        {
+                            cmd.CommandText = "INSERT INTO Calendar (UserId) VALUES(@user_Id)";
+                            cmd.Parameters.AddWithValue("UserId", calendar.UserId);
+                            
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+                    catch (SqlException e)
+                    {
+                        throw e;
+                    }
+                }
+                scope.Complete();
+            }
+        }
+    }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Calendar Get(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Calendar> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Calendar entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
