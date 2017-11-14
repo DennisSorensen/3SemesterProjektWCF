6789bbb2202c362541c296a81ffd900258e87936
Bodil <bodil.noergaard86@gmail.com>
2017-11-14 09:43:26 +0100

    class calendar

---
+++ WCF.ModelLayer/Calendar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCF.ModelLayer
+{
+    public class Calendar
+    {
+        public int Id { get; set; }
+        public int UserId { get;}
+        public int BookingId { get; set; }
+
+        public Calendar(int id, int userId)
+        {
+            this.Id = id;
+            this.UserId = userId;
+        }
+    }
+}
