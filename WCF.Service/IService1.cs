b6df64d249cf54c8bee576460c6f1dbd7b76ed62
Jesper Hesthaven <Junkus@live.dk>
2017-11-17 09:37:04 +0100

    Working Service

--- WCF.Service/IService1.cs
+++ WCF.Service/IService1.cs
@@ -3,23 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
+using System.ServiceModel.Web;
 using System.Text;
 using WCF.ModelLayer;
 
 namespace WCF.Service
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IUserService" in both code and config file together.
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
-    public interface IUserService
+    public interface IService1
     {
-
         [OperationContract]
         bool CreateUser(User user);
 
         [OperationContract]
         User GetUser(int id);
-
-        [OperationContract]
-        User GetAllSupporters();
+        
+        // TODO: Add your service operations here
     }
 }
--- WCF.Service/Service1.svc.cs
+++ WCF.Service/Service1.svc.cs
@@ -3,17 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
+using System.ServiceModel.Web;
 using System.Text;
-using WCF.BusinessLogicLayer;
 using WCF.ModelLayer;
+using WCF.BusinessLogicLayer;
 
 namespace WCF.Service
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "UserService" in code, svc and config file together.
-    // NOTE: In order to launch WCF Test Client for testing this service, please select UserService.svc or UserService.svc.cs at the Solution Explorer and start debugging.
-    public class UserService : IUserService
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
     {
         private UserController userController = new UserController();
+       
 
         public bool CreateUser(User user)
         {
@@ -29,5 +31,6 @@ namespace WCF.Service
         {
             return null;
         }
+
     }
 }
