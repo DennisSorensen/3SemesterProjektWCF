35a9324fe491c08217f12577cead77da3da129d5
Bodil <bodil.noergaard86@gmail.com>
2017-11-22 13:17:29 +0100

    modellag klasser færdig

---
+++ WCF.ModelLayer/SupportBooking.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCF.ModelLayer
+{
+    public class SupportBooking : Booking
+    {
+        
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Phone { get; set; }
+        public string Description { get; set; }
+
+        public SupportBooking(int id, DateTime startDate, DateTime endDate, string bookingType, int user_Id, string firstName, string lastName, int phone, string description) : base(id, startDate, endDate, bookingType, user_Id)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Phone = phone;
+            this.Description = description;
+        }
+
+    }
+}
---
+++ WCF.ModelLayer/Task.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCF.ModelLayer
+{
+    public class Task : Booking
+    {
+        public Task(int id, DateTime startDate, DateTime endDate, string bookingType, int user_Id, string name, string description) : base(id, startDate, endDate, bookingType, user_Id)
+        {
+            this.Name = name;
+            this.Description = description;
+        }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
