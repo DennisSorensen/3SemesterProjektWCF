db045b93eee79d5c9993fece704eba3f0e8287ba
Jesper Hesthaven <Junkus@live.dk>
2017-11-24 09:03:49 +0100

    Added Stuff

--- WCF.Service/BookingService.svc.cs
+++ WCF.Service/BookingService.svc.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using WCF.BusinessLogicLayer;
+using WCF.ModelLayer;
 
 namespace WCF.Service
 {
@@ -11,8 +13,23 @@ namespace WCF.Service
     // NOTE: In order to launch WCF Test Client for testing this service, please select BookingService.svc or BookingService.svc.cs at the Solution Explorer and start debugging.
     public class BookingService : IBookingService
     {
-        public void DoWork()
+        private TaskController taskController = new TaskController();
+        private SupportBookingController supportBookingController = new SupportBookingController();
+        private ReadyToGoController readyToGoController = new ReadyToGoController();
+        public void CreateReadyToGo(ReadyToGo readyToGo)
         {
+            readyToGoController.Create(readyToGo);
         }
+
+        public void CreateSupportBooking(SupportBooking supportBooking)
+        {
+            supportBookingController.Create(supportBooking);
+        }
+
+        public void CreateSupportTask(SupportTask supportTask)
+        {
+            taskController.Create(supportTask);
+        }
+        
     }
 }
